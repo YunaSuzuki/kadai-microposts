c2e3c27e02fc3725b57eb16ab49a23d771930ddc
yuna.suzuki <yuna.com.jp@outlook.jp>
2019-11-21 07:13:57 +0000

    dependent destroy (favorites)

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,13 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
     has_secure_password
     
-    has_many :microposts
-    has_many :relationships
-    has_many :followings, through: :relationships, source: :follow
-    has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
-    has_many :followers, through: :reverses_of_relationship, source: :user
-    has_many :favorites
-    has_many :favoritepost, through: :favorites, source: :micropost
+    has_many :microposts, dependent: :destroy
+    has_many :relationships, dependent: :destroy
+    has_many :followings, through: :relationships, source: :follow, dependent: :destroy
+    has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id', dependent: :destroy
+    has_many :followers, through: :reverses_of_relationship, source: :user, dependent: :destroy
+    has_many :favorites, dependent: :destroy
+    has_many :favoritepost, through: :favorites, source: :micropost, dependent: :destroy
     
     #ここではfollow/unfollowに関して使うメソッドを全て定義
     def follow(other_user)
